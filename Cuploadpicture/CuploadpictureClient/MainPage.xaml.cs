908745d0afdd7b43c7017e967a7ab20727c537cf
sunteenwu <wu_15xun@163.com>
2016-05-16 15:18:43 +0800

    Upload picture demo

---
+++ Cuploadpicture/CuploadpictureClient/MainPage.xaml.cs
@@ -0,0 +1,114 @@
+﻿ 
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.ServiceModel;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Display;
+using Windows.Graphics.Imaging;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CuploadpictureClient
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        public async Task<ImageSource> SaveToImageSource(byte[] imageBuffer)
+        {
+            ImageSource imageSource = null;
+            using (MemoryStream stream = new MemoryStream(imageBuffer))
+            {
+                var ras = stream.AsRandomAccessStream();
+                BitmapDecoder decoder = await BitmapDecoder.CreateAsync(BitmapDecoder.JpegDecoderId, ras);
+                var provider = await decoder.GetPixelDataAsync();
+                byte[] buffer = provider.DetachPixelData();
+                WriteableBitmap bitmap = new WriteableBitmap((int)decoder.PixelWidth, (int)decoder.PixelHeight);
+                await bitmap.PixelBuffer.AsStream().WriteAsync(buffer, 0, buffer.Length);
+                imageSource = bitmap;
+            }
+            return imageSource;
+        }
+        public async Task<byte[]> SaveToBytesAsync(ImageSource imageSource)
+        {
+            byte[] imageBuffer;
+            var localFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+            var file = await localFolder.CreateFileAsync("temp.jpg", CreationCollisionOption.ReplaceExisting);
+            using (var ras = await file.OpenAsync(FileAccessMode.ReadWrite, StorageOpenOptions.None))
+            {
+                WriteableBitmap bitmap = imageSource as WriteableBitmap;
+                var stream = bitmap.PixelBuffer.AsStream();
+                byte[] buffer = new byte[stream.Length];
+                await stream.ReadAsync(buffer, 0, buffer.Length);
+                BitmapEncoder encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.JpegEncoderId, ras);
+                encoder.SetPixelData(BitmapPixelFormat.Bgra8, BitmapAlphaMode.Ignore, (uint)bitmap.PixelWidth, (uint)bitmap.PixelHeight, 96.0, 96.0, buffer);
+                await encoder.FlushAsync();
+
+                var imageStream = ras.AsStream();
+                imageStream.Seek(0, SeekOrigin.Begin);
+                imageBuffer = new byte[imageStream.Length];
+                var re = await imageStream.ReadAsync(imageBuffer, 0, imageBuffer.Length);
+
+            }
+            await file.DeleteAsync(StorageDeleteOption.Default);
+            return imageBuffer;
+        }       
+        private async void Convert_Click(object sender, RoutedEventArgs e)
+        {
+            FileOpenPicker fileOpenPicker = new FileOpenPicker();
+            fileOpenPicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+            fileOpenPicker.FileTypeFilter.Add(".jpg");
+            fileOpenPicker.ViewMode = PickerViewMode.Thumbnail;
+
+            var inputFile = await fileOpenPicker.PickSingleFileAsync();
+
+            if (inputFile == null)
+            {
+                // The user cancelled the picking operation
+                return;
+            }
+            SoftwareBitmap softwareBitmap;
+            using (IRandomAccessStream stream = await inputFile.OpenAsync(FileAccessMode.Read))
+            {
+                // Create the decoder from the stream
+                BitmapDecoder decoder = await BitmapDecoder.CreateAsync(stream);
+                // Get the SoftwareBitmap representation of the file
+                softwareBitmap = await decoder.GetSoftwareBitmapAsync();
+            }
+            //var bitmap = new SoftwareBitmap(BitmapPixelFormat.Bgra8, 200, 200);
+            var bitmap = softwareBitmap;
+            var imgSource = new WriteableBitmap(bitmap.PixelWidth, bitmap.PixelHeight);
+            imgbit.Source = imgSource;
+            //SaveToBytesAsync(imgSource);
+            bitmap.CopyToBuffer(imgSource.PixelBuffer); 
+            byte[] dataArrayTobeSent=await SaveToBytesAsync(imgSource);            
+        }
+         
+    }
+}
