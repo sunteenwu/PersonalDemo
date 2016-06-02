a8bcb8de46ece4a22bdfd19c3628840cd6301425
sunteenwu <wu_15xun@163.com>
2016-06-02 17:32:48 +0800

    New demo for extended splash screen

--- CSplashScreen/CSplashScreen/ExtendedSplash.xaml.cs
+++ CSplashScreen/CSplashScreen/ExtendedSplash.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -23,30 +24,21 @@ namespace CSplashScreen
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    partial class ExtendedSplash : Page
+    public sealed partial class ExtendedSplash: Page
     {
-
-        string text = "800";
-
         internal Rect splashImageRect; // Rect to store splash screen image coordinates.
         private SplashScreen splash; // Variable to hold the splash screen object.
         internal bool dismissed = false; // Variable to track splash screen dismissal status.
         internal Frame rootFrame;
-        //https://msdn.microsoft.com/zh-cn/library/jj819807.aspx
-
-
+        private readonly DispatcherTimer _timer;
+        //Make a place to store the last time the displayed item was set
+        private DateTime _lastChange;
         public ExtendedSplash(SplashScreen splashscreen, bool loadState)
         {
-            this.DataContext = text;
-            InitializeComponent();
-            ApplicationView.PreferredLaunchViewSize = new Size(800, 800);
-            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
-
-            storyboard.Completed += MyStoryboard_Completed;
+            this.InitializeComponent();
             // Listen for window resize events to reposition the extended splash screen image accordingly.
             // This is important to ensure that the extended splash screen is formatted properly in response to snapping, unsnapping, rotation, etc...
             Window.Current.SizeChanged += new WindowSizeChangedEventHandler(ExtendedSplash_OnResize);
-            splash = splashscreen;
             if (splash != null)
             {
                 // Register an event handler to be executed when the splash screen has been dismissed.
@@ -58,52 +50,43 @@ namespace CSplashScreen
             }
 
             // Create a Frame to act as the navigation context
-            rootFrame = new Frame();
-
-            // Restore the saved session state if necessary
-            RestoreStateAsync(loadState);
-        }
-
-        private void MyStoryboard_Completed(object sender, object e)
-        {
-            // Navigate to mainpage
-            rootFrame.Navigate(typeof(MainPage));
-            // Place the frame in the current Window
-            Window.Current.Content = rootFrame;
-        }
-
-        void RestoreStateAsync(bool loadState)
-        {
-            if (loadState)
+            rootFrame = new Frame();       
+          
+            _timer = new DispatcherTimer
             {
-                // TODO: write code to load state
-            }
+                //Set the interval between ticks (in this case 2 seconds to see it working)
+                Interval = TimeSpan.FromSeconds(2)
+            };
+
+            //Change what's displayed when the timer ticks
+            _timer.Tick += ChangeImage;
+            //Start the timer
+            _timer.Start();
         }
+
+        private void ChangeImage(object sender, object e)
+        {
+            //Get the number of items in the flip view
+            var totalItems = ImageFlipView.Items.Count;
+            //Figure out the new item's index (the current index plus one, if the next item would be out of range, go back to zero)
+            var newItemIndex = (ImageFlipView.SelectedIndex + 1) % totalItems;
+            //Set the displayed item's index on the flip view
+            ImageFlipView.SelectedIndex = newItemIndex;
+        }  
+    
 
         // Position the extended splash screen image in the same location as the system splash screen image.
         void PositionImage()
         {
-            SplashScreenImage.SetValue(Canvas.LeftProperty, splashImageRect.X);
-            //System.Diagnostics.Debug.WriteLine(splashImageRect.X);
-            SplashScreenImage.SetValue(Canvas.TopProperty, splashImageRect.Y);
-            System.Diagnostics.Debug.WriteLine(splashImageRect.Y);
-            SplashScreenImage.Height = splashImageRect.Height;
-            System.Diagnostics.Debug.WriteLine(splashImageRect.Height);
-            SplashScreenImage.Width = splashImageRect.Width;
-            storyboard.Begin();
+            ImageFlipView.Height = ContentGrid.ActualHeight;
+            ImageFlipView.Width = ContentGrid.ActualWidth;
         }
-
 
         void ExtendedSplash_OnResize(Object sender, WindowSizeChangedEventArgs e)
         {
-            // Safely update the extended splash screen image coordinates. This function will be fired in response to snapping, unsnapping, rotation, etc...
-            if (splash != null)
-            {
-                // Update the coordinates of the splash screen image.
-                splashImageRect = splash.ImageLocation;
+            
                 PositionImage();
-
-            }
+            
         }
 
         // Include code to be executed when the system has transitioned from the splash screen to the extended splash screen (application's first view).
@@ -113,13 +96,16 @@ namespace CSplashScreen
             // Complete app setup operations here...
         }
 
-        void DismissExtendedSplash()
+        private void Page_Loaded(object sender, RoutedEventArgs e)
         {
-            // Navigate to mainpage
-            rootFrame.Navigate(typeof(MainPage));
+            PositionImage();
+        }
+
+        private void BtnDismissSplash_Click(object sender, RoutedEventArgs e)
+        {
+            rootFrame.Navigate(typeof(MainPage));    
             // Place the frame in the current Window
             Window.Current.Content = rootFrame;
         }
-
     }
 }
