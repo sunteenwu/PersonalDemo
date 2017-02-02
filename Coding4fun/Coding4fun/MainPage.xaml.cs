f79637e0864c0be059426e959593bc9cb08743fa
sunteenwu <wu_15xun@163.com>
2017-02-02 14:24:40 +0800

    Add a new project

---
+++ Coding4fun/Coding4fun/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Coding4fun
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void btnCPopen_Click(object sender, RoutedEventArgs e)
+        {
+            CPtest.Visibility = Visibility;
+        }
+        private void CPtest_ColorChanged(object sender, Windows.UI.Color color)
+        {
+            BorCP.Background = new SolidColorBrush(color);
+        }
+    }
+}
